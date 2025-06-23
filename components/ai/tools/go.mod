f237a7a9ddc9aae83bcebebb42ba7150c1b35226
elewis787 <ethanlewis787@gmail.com>
2025-06-23 11:04:42 -0700

    feat: add empty tool box

---
+++ components/ai/tools/go.mod
@@ -0,0 +1,5 @@
+module github.com/hayride-dev/morphs/components/ai/tools
+
+go 1.23.6
+
+require go.bytecodealliance.org/cm v0.2.2
--- components/ai/tools/tools.go
+++ components/ai/tools/tools.go
@@ -1,7 +1,47 @@
 package main
 
-func init() {}
+import (
+	"unsafe"
+
+	"github.com/hayride-dev/morphs/components/ai/tools/internal/gen/hayride/ai/tools"
+	"go.bytecodealliance.org/cm"
+)
+
+type resources struct {
+	tools map[cm.Rep]*noopTools
+}
+
+var resourceTable = &resources{
+	tools: make(map[cm.Rep]*noopTools),
+}
+
+func init() {
+	tools.Exports.Tools.Constructor = constructor
+	tools.Exports.Tools.Call = call
+	tools.Exports.Tools.Capabilities = capabilities
+	tools.Exports.Tools.Destructor = destructor
+}
 
 type noopTools struct{}
+
+func constructor(t cm.List[tools.ToolSchema]) tools.Tools {
+	noop := &noopTools{}
+	key := cm.Rep(uintptr(*(*unsafe.Pointer)(unsafe.Pointer(&noop))))
+	v := tools.ToolsResourceNew(key)
+	resourceTable.tools[key] = noop
+	return v
+}
+
+func call(self cm.Rep, input tools.ToolInput) (result cm.Result[tools.ToolOutputShape, tools.ToolOutput, tools.ErrorCode]) {
+	return cm.OK[cm.Result[tools.ToolOutputShape, tools.ToolOutput, tools.ErrorCode]](tools.ToolOutput{})
+}
+
+func capabilities(self cm.Rep) (result cm.Result[cm.List[tools.ToolSchema], cm.List[tools.ToolSchema], tools.Error]) {
+	return cm.OK[cm.Result[cm.List[tools.ToolSchema], cm.List[tools.ToolSchema], tools.Error]](cm.List[tools.ToolSchema]{})
+}
+
+func destructor(self cm.Rep) {
+	delete(resourceTable.tools, self)
+}
 
 func main() {}
